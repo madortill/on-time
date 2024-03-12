6be9de23d7a979afa19e4442b0a67f6770ba10bb
madortill <madortill@gmail.com>
2024-03-12 15:08:16 +0200

    migrate from ccreate-react to vite

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import { DesignProvider } from "./contexts/design.context";
+import { MeetingProvider } from "./contexts/meeting.context";
+import { CounterProvider } from "./contexts/counter.context";
+import { TimerProvider } from './contexts/timer.context';
+import './index.css';
+import App from './App.jsx';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <BrowserRouter>
+      <TimerProvider>
+        <DesignProvider>
+          <MeetingProvider>
+            <CounterProvider>
+              <App />
+            </CounterProvider>
+          </MeetingProvider>
+        </DesignProvider>
+      </TimerProvider>
+    </BrowserRouter>
+  </React.StrictMode>
+);
